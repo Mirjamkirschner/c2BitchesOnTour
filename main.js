800ae64f1d329b7fa6771c7b3a5135039a14b953
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-07-04 17:09:40 +0200

    quellen startseite entfernt unten im footer

--- main.js
+++ main.js
@@ -15,9 +15,8 @@ let map = L.map("map", {
 // thematische Layer
 let themaLayer = {
     innsbruckLipno: L.featureGroup(),
+    muenchenVenedig: L.featureGroup(),
     forecast: L.featureGroup(),
-    badeseen: L.featureGroup(),
-    eisdielen: L.featureGroup()
 };
 
 // Hintergrundlayer 
@@ -27,9 +26,8 @@ let layerControl = L.control.layers({
     "CycleTrails": L.tileLayer.provider("CyclOSM"),
 }, {
     "Innsbruck-Lipno": themaLayer.innsbruckLipno.addTo(map),
+    "München-Venedig": themaLayer.muenchenVenedig.addTo(map),
     "Wettervorhersage MET Norway": themaLayer.forecast,
-    "Badeseen": themaLayer.badeseen,
-    "Eisdielen": themaLayer.eisdielen,
 }).addTo(map);
 
 // Layer beim Besuch auf der Seite ausklappen
@@ -133,7 +131,7 @@ kamp.on("click", function (evt) {
         theme: "innsbruck-gerlos"
     }).addTo(map);
     // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./gpx/Innsbruck_Gerlos.gpx")
+    controlElevation.load("./tracks/Innsbruck_Gerlos.gpx")
 });
 
 
@@ -326,6 +324,241 @@ ybbs.on("click", function (evt) {
     controlElevation.load("./tracks/Linz_Lipno.gpx")
 });
 
+//München-Fall
+var gpx = './tracks/muenchenFall.gpx';
+let muenchen = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#0000ee',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+muenchen.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "muenchen-fall"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/muenchenFall.gpx")
+});
+
+//Fall-Innsbruck
+var gpx = './tracks/fallInnsbruck.gpx';
+let fall = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#ff7f24',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+fall.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/fallInnsbruck.gpx")
+});
+
+//Innsbruck-Sterzing
+var gpx = './tracks/innsbruckSterzing.gpx';
+let innsbruck = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#0000ee',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+innsbruck.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/innsbruckSterzing.gpx")
+});
+
+
+//Sterzing-Bruneck
+var gpx = './tracks/sterzingBruneck.gpx';
+let sterzing = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#ff7f24',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+kamp.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/sterzingBruneck.gpx")
+});
+
+//Bruneck-Cortina
+var gpx = './tracks/bruneckCortina.gpx';
+let bruneck = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#0000ee',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+bruneck.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/bruneckCortina.gpx")
+});
+
+//Cortina-Farra
+var gpx = './tracks/cortinaFarra.gpx';
+let cortina = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#ff7f24',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+cortina.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/cortinaFarra.gpx")
+});
+
+//Farra-Treviso
+var gpx = './tracks/farraTreviso.gpx';
+let farra = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#0000ee',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+farra.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/farraTreviso.gpx")
+});
+
+//Treviso-Venedig
+var gpx = './tracks/trevisoVenezia.gpx';
+let treviso = new L.GPX(gpx, {
+    polyline_options: {
+        color: '#ff7f24',
+        opacity: 0.75,
+        weight: 6
+    },
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false,
+        wptIconUrls: false
+    }
+}).addTo(themaLayer.muenchenVenedig);
+
+
+// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
+treviso.on("click", function (evt) {
+    let controlElevation = L.control.elevation({
+        time: false,
+        elevationDiv: "#profile",
+        height: 300,
+        theme: "innsbruck-gerlos"
+    }).addTo(map);
+    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
+    controlElevation.load("./tracks/trevisoVenezia.gpx")
+});
+
+
+
 // // Marker der größten Städte
 // const STAEDTE = [
 //     {
@@ -362,89 +595,89 @@ ybbs.on("click", function (evt) {
 //     `)
 // };
 
-//Badeseen
-const BADESEEN = [
-    {
-        title: "Ottensteiner Stausee",
-        lat: 48.61799121352252,
-        lng: 15.267483226467856,
-    },
-    {
-        title: "Bernhardsthaler Teich",
-        lat: 48.692857068868165,
-        lng: 16.88281412284728
-    },
-    {
-        title: "Naturbadeseen Traismauer",
-        lat: 48.3654889628816,
-        lng: 15.753145918954235
-    },
-    {
-        title: "Badeteich Kalte Kuchl",
-        lat: 47.888507360144025,
-        lng: 15.68338462871469
-    },
-    {
-        title: "Badeteich Persenbeug-Gottsdorf",
-        lat: 48.18540724140658,
-        lng: 15.103914278527622
-    }
-];
+// //Badeseen
+// const BADESEEN = [
+//     {
+//         title: "Ottensteiner Stausee",
+//         lat: 48.61799121352252,
+//         lng: 15.267483226467856,
+//     },
+//     {
+//         title: "Bernhardsthaler Teich",
+//         lat: 48.692857068868165,
+//         lng: 16.88281412284728
+//     },
+//     {
+//         title: "Naturbadeseen Traismauer",
+//         lat: 48.3654889628816,
+//         lng: 15.753145918954235
+//     },
+//     {
+//         title: "Badeteich Kalte Kuchl",
+//         lat: 47.888507360144025,
+//         lng: 15.68338462871469
+//     },
+//     {
+//         title: "Badeteich Persenbeug-Gottsdorf",
+//         lat: 48.18540724140658,
+//         lng: 15.103914278527622
+//     }
+// ];
 
-for (let badeseen of BADESEEN) {
-    L.marker([badeseen.lat, badeseen.lng], {
-        icon: L.icon({
-            iconUrl: `icons/swimming.png`,
-            popupAnchor: [0, -37],
-            iconAnchor: [16, 37],
-        })
-    })
-        .addTo(themaLayer.badeseen)
-        .bindPopup(`<b>${badeseen.title}</b> <br>
-    `)
-};
+// for (let badeseen of BADESEEN) {
+//     L.marker([badeseen.lat, badeseen.lng], {
+//         icon: L.icon({
+//             iconUrl: `icons/swimming.png`,
+//             popupAnchor: [0, -37],
+//             iconAnchor: [16, 37],
+//         })
+//     })
+//         .addTo(themaLayer.badeseen)
+//         .bindPopup(`<b>${badeseen.title}</b> <br>
+//     `)
+// };
 
-//Eisdielen
-const EISDIELEN = [
-    {
-        title: "Muratti Gelateria",
-        lat: 47.92887177060042, 
-        lng: 16.216587179951677,
-    },
-    {
-        title: "Eisdiele Papa Luigi",
-        lat: 48.05055989884008, 
-        lng: 15.612404277191253,
-    },
-    {
-        title: "Eiscafé Venezia",
-        lat: 48.12287154575264,
-        lng: 14.872194820122328,
-    },
-    {
-        title: "Eissalon Daniel",
-        lat: 48.147208935083064, 
-        lng: 16.940528019026875,
-    },
-    {
-        title: "Eissalon Mazzer Renzo",
-        lat: 48.41104791919906,
-        lng: 15.602391376465306,
-    }
-];
+// //Eisdielen
+// const EISDIELEN = [
+//     {
+//         title: "Muratti Gelateria",
+//         lat: 47.92887177060042,
+//         lng: 16.216587179951677,
+//     },
+//     {
+//         title: "Eisdiele Papa Luigi",
+//         lat: 48.05055989884008,
+//         lng: 15.612404277191253,
+//     },
+//     {
+//         title: "Eiscafé Venezia",
+//         lat: 48.12287154575264,
+//         lng: 14.872194820122328,
+//     },
+//     {
+//         title: "Eissalon Daniel",
+//         lat: 48.147208935083064,
+//         lng: 16.940528019026875,
+//     },
+//     {
+//         title: "Eissalon Mazzer Renzo",
+//         lat: 48.41104791919906,
+//         lng: 15.602391376465306,
+//     }
+// ];
 
-for (let eisdielen of EISDIELEN) {
-    L.marker([eisdielen.lat, eisdielen.lng], {
-        icon: L.icon({
-            iconUrl: `icons/icecream.png`,
-            popupAnchor: [0, -37],
-            iconAnchor: [16, 37],
-        })
-    })
-        .addTo(themaLayer.eisdielen)
-        .bindPopup(`<b>${eisdielen.title}</b> <br>
-    `)
-};
+// for (let eisdielen of EISDIELEN) {
+//     L.marker([eisdielen.lat, eisdielen.lng], {
+//         icon: L.icon({
+//             iconUrl: `icons/icecream.png`,
+//             popupAnchor: [0, -37],
+//             iconAnchor: [16, 37],
+//         })
+//     })
+//         .addTo(themaLayer.eisdielen)
+//         .bindPopup(`<b>${eisdielen.title}</b> <br>
+//     `)
+// };
 
 
 // Maßstab
